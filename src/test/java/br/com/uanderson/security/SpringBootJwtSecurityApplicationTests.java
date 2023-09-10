3639b1a3c107bb00861ad9f8a55af8942d7ec010
Uanderson Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2023-09-10 15:53:26 -0300

    feat: criando a estrutura inicial do projeto

---
+++ src/test/java/br/com/uanderson/security/SpringBootJwtSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uanderson.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
